82393d0a47d981e7404f4f43d1954ef71d1d3829
ritish bhardwaj <ritishbhardwaj02@gmail.com>
2023-01-07 12:27:56 +0530

    'day6'

---
+++ GoldmanSachs/Factorial with Trailing Zeroes.py
@@ -0,0 +1,33 @@
+# 3.
+
+from functools import lru_cache
+class Solution:
+    #======================Approach 1 ==================
+    def trailingZeroes(self, n: int) -> int:
+
+        @lru_cache
+        def fact(n):
+            if n==0:
+                return 1
+            if n==1:
+                return 1
+            if fact==2:
+                return 2
+            else:
+                return n*fact(n-1)
+        ans=fact(n)
+        i=10
+        count=0
+        while True:
+            if ans % i == 0:
+                i*=10
+                count+=1
+            else:
+                break
+        return count
+
+
+obj=Solution()
+n=4544
+ans=obj.trailingZeroes(n)
+print(ans)
